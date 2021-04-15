bc701e187581159ab686b1f3bd3f8dfe769926bd
xnkasy <xnkasypriyanshu@gmail.com>
2021-04-16 01:59:07 +0530

    Extra 0 at the end, setting to null should work

--- join1.cpp
+++ join1.cpp
@@ -30,17 +30,17 @@ int main(int argc, const char* argv[]) {
 	int endPage2= ph.GetPageNum();
 	file2.UnpinPage(endPage2);
     vector<int> ans;
-    for(int page1 = startPage1; page1 < endPage1; ++page1){
+    for(int page1 = startPage1; page1 <=endPage1; ++page1){
         PageHandler pg1 = file1.PageAt(page1);
         char* ptrToData1 = pg1.GetData();
         int * data1 = (int *)ptrToData1;
-        for(int page2 = startPage2; page2 < endPage2; ++page2){
+        for(int page2 = startPage2; page2 <= endPage2; ++page2){
             PageHandler pg2 = file2.PageAt(page2);
             char* ptrToData2 = pg2.GetData();
             int * data2 = (int *)ptrToData2;
             for(int i = 0; i < PAGE_CONTENT_SIZE/sizeof(int); ++i){
                 for(int j = 0; j< PAGE_CONTENT_SIZE/sizeof(int); ++j){
-                    if(data1==NULL or data2==NULL){
+                    if(&data1[i]==NULL or &data2[j]==NULL){
                         break;
                     }
                     if(data1[i]==data2[j]){
@@ -55,63 +55,32 @@ int main(int argc, const char* argv[]) {
     for(int i = 0; i< ans.size(); ++i){
         cout<<ans[i]<<" "<<endl;
     }
-	ans.clear();
+    cout<<ans.size();
 	// Close the files
 	fm.CloseFile(file1);
     fm.CloseFile(file2);
+
+    FileHandler outFile=fm.CreateFile(argv[3]);	
+	PageHandler outPage = outFile.NewPage ();
+	char *outptr = outPage.GetData ();
+    int j = 0, pageNum = 0;
+    for(int i = 0; i<ans.size(); ++i){
+        memcpy(&outptr[j], &ans[i], sizeof(int));
+		j=j+sizeof(int);
+        if(j>=PAGE_CONTENT_SIZE){
+            outFile.UnpinPage(pageNum);
+            outPage = outFile.NewPage();
+            outptr = outPage.GetData();
+            j=0;
+            pageNum++;
+        }
+    }
+    cout<<"Here, j is "<<j<<endl;
+    outptr[j] = NULL;
+    // if(j<PAGE_CONTENT_SIZE){
+    //     memcpy(&outptr[j], NULL, sizeof(int));
+    // }
+    fm.CloseFile(outFile);
+    ans.clear();
     return 0;
 }
-
-// int main(int argc, const char* argv[]) {
-// 	FileManager fm;
-// 	int i=0, pageno=0;
-// 	// Create a brand new file
-// 	while(true)
-// 	{
-// 		cout<<"entry"<<endl;
-// 		FileHandler fh = fm.OpenFile(argv[2]);
-// 		PageHandler ph = fh.PageAt(pageno);
-// 		char* ptrToNum=ph.GetData();
-// 		int num;
-// 		if(8*i+11<=PAGE_CONTENT_SIZE)
-// 		{
-// 			memcpy (&num, &ptrToNum[8*i+7], sizeof(int));
-// 			i++;
-// 		}
-// 		else
-// 		{
-// 			i=0;
-// 			pageno++;
-// 			ph=fh.LastPage();
-// 			if(ph.GetPageNum()==pageno)
-// 				break;
-// 			memcpy (&num, &ptrToNum[8*i+7], sizeof(int));
-// 		}
-// 		ph=fh.LastPage();
-// 		if(ph.GetPageNum()==pageno)
-// 			break;
-// 		fm.CloseFile(fh);
-
-// 		fh = fm.OpenFile(argv[1]);
-// 		cout << "File opened " << endl;
-		
-// 		// Create a new page
-// 		ph = fh.FirstPage();
-// 		int startpage= ph.GetPageNum();
-// 		fh.UnpinPage(startpage);
-// 		ph = fh.LastPage();
-// 		int endpage= ph.GetPageNum();
-// 		fh.UnpinPage(endpage);
-		
-// 		int mid, offset; //change to vectors
-// 		cout<<"entering"<<endl;
-// 		bool ans=bsearch(startpage, endpage, &mid, &offset, num, fh);
-
-// 		if(ans){
-// 			cout << (mid+1)<<","<<(offset+1)<<endl;
-// 		}
-// 		cout << "-1,-1"<<endl;
-// 		// Close the file
-// 		fm.CloseFile(fh);
-// 	}
-// }
